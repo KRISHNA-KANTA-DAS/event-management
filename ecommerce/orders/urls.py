ce28abb1e0a5bf7613873ff2c526a44a6d3758fc
KRISHNA KANTA DAS <krishnakanta.it.aec@gmail.com>
2026-02-11 20:10:55 +0530

    Delete ecommerce directory

--- ecommerce/orders/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('cart/', views.cart_view, name='cart'),
-    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    path('checkout/', views.checkout, name='checkout'),
-]
